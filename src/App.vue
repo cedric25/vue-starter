59acab4ddcf1d6d9cd4246230d7d2191ca3dca2b
Cedric Nicoloso <cedric.nicoloso@iex.ec>
2023-10-27 13:58:39 +0200

    Center Hello World!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="p-3">
-    <h1 class="text-lg">
-      Hello world!
-    </h1>
+    <h1 class="text-2xl flex items-center justify-center h-[50vh]">Hello world!</h1>
   </div>
 </template>
